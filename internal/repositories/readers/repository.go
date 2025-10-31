5a843e2a4dfa5f5ed55e96d92183d5490d0030c9
JupiterPanda <iterin2003@gmail.com>
2025-10-31 16:47:31 +0400

    bugfix (change repo create func)

--- internal/repositories/readers/repository.go
+++ internal/repositories/readers/repository.go
@@ -21,7 +21,7 @@ func NewRepo(db *pgxpool.Pool) *Repository {
 
 func (r *Repository) Create(ctx context.Context, reader models.Reader) error {
 	query := `INSERT INTO readers (name, number, address, date_of_birth) VALUES ($1, $2, $3, $4)`
-	err := r.db.QueryRow(ctx, query, reader.Name, reader.PhoneNumber, reader.Address, reader.DateOfBirth)
+	_, err := r.db.Exec(ctx, query, reader.Name, reader.PhoneNumber, reader.Address, reader.DateOfBirth)
 	if err != nil {
 		// TODO Проверка на вставку дубликата.
 		return fmt.Errorf("[repo][Create] ошибка при запросе в БД: %w", err)
